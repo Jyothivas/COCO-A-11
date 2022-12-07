2e6e91979e850cfa9cc8df1f7744f6792acd149e
Karan <karanmhetre9@gmail.com>
2022-12-07 22:15:30 +0530

    Project1 New Employee form is completed. and datatable is also completed data is inserted as per requirement.

--- HTML_CSS/Project1/form.js
+++ HTML_CSS/Project1/form.js
@@ -1,36 +1,50 @@
-    let n= data.length;
-    var index=data.length-1;
 
-    function AddRow(){
-        console.log(data.length);
-        let addRow = document.getElementById('example');
-        let NewRow = addRow.insertRow(n);
 
-        data[index][0] = n;
-        data[index][1] = document.getElementById('email').value;
-        data[index][2] = document.getElementById('age').value;
-        data[index][3] = document.querySelector('input[name="gender"]:checked').value;
-        data[index][4] = "<a href=''>Edit</a> / <a href=''>Delete</a>";;
-        console.log(data[index]);
-
-        
-        let cell1 = NewRow.insertCell(0);
-        let cell2 = NewRow.insertCell(1);
-        let cell3 = NewRow.insertCell(2);
-        let cell4 = NewRow.insertCell(3);
-        let cell5 = NewRow.insertCell(4);
-
-        cell1.innerHTML = data[index][0];
-        cell2.innerHTML = data[index][1];
-        cell3.innerHTML = data[index][2];
-        cell4.innerHTML = data[index][3];
-        cell5.innerHTML = data[index][4]; 
-        n++;
-        
-        
-    } 
-
-    function deleteData(id){
-
-    }
  
+function AddRow(){
+    let srNum =employeeData.length+1;
+    var Email = document.getElementById('email').value;
+    var Age = document.getElementById('age').value;
+    var Gender = document.querySelector('input[name="gender"]:checked').value;
+    var Action = "<button class='btn btn-success'>Edit</button> <button class='btn btn-danger' onclick='deleteEmployee(srNUM)'>Delete</button>";;
+    
+        rows=[srNum,Email,Age,Gender,Action]
+    
+        employeeData.push(rows);
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*  function deleteEmployee(empId){
+
+    console.log(empId);
+    console.log(employeeData);
+    employeeData=employeeData.filter(employeeData[empId]);
+    console.log(employeeData)
+}  */
