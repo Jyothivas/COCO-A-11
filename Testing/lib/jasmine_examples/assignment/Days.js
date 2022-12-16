f032f1bf67638476d682d669fd5949ff451dce46
Karan <karanmhetre9@gmail.com>
2022-12-16 23:21:13 +0530

    TDD for week days complete.

--- Testing/lib/jasmine_examples/assignment/Days.js
+++ Testing/lib/jasmine_examples/assignment/Days.js
@@ -1,10 +1,10 @@
 class Days{
     dayOfWeek(number){
-        let week =['Sunday','Monday','Tuesday','Wednesday','Thrusday','Friday','Saturday'];
-        for(let index=0;index<week.length;index++){
-            if(number==index)
-                return week[index-1];
+        if(number>0 && number<8){
+            let week =['Sunday','Monday','Tuesday','Wednesday','Thrusday','Friday','Saturday'];
+                return week[number-1];
         }
+        return "number is not less than 1 or greater than 7";
     }
 }
 
--- Testing/spec/assignment/DaysSpec.js
+++ Testing/spec/assignment/DaysSpec.js
@@ -4,6 +4,7 @@ describe('Week days',function(){
 
     it('Name of the Day for a given day of the week',function(){
         const days=new Days();
+        expect(days.dayOfWeek(0)).toEqual('number is not less than 1 or greater than 7');
         expect(days.dayOfWeek(1)).toEqual('Sunday');
         expect(days.dayOfWeek(2)).toEqual('Monday');
         expect(days.dayOfWeek(3)).toEqual('Tuesday');
@@ -11,5 +12,8 @@ describe('Week days',function(){
         expect(days.dayOfWeek(5)).toEqual('Thrusday');
         expect(days.dayOfWeek(6)).toEqual('Friday');
         expect(days.dayOfWeek(7)).toEqual('Saturday');
+        expect(days.dayOfWeek(8)).toEqual('number is not less than 1 or greater than 7');
+
+
     });
 });
