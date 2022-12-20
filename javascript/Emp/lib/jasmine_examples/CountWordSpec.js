42adfdab71d00b2c05d7dacaecb71c09dc0daf90
Monika Tilekar <monikatilekar80@gmail.com>
2022-12-20 20:22:30 +0530

    countword

---
+++ javascript/Emp/lib/jasmine_examples/CountWordSpec.js
@@ -0,0 +1,11 @@
+const CountWord = require('../../lib/jasmine_examples/CountWord')
+
+describe('Count Word', function(){
+
+    it('Total words in a string', function(){
+        const countWord = new CountWord();
+        expect(countWord.strWord('This project is done')).toEqual(4);
+        expect(countWord.strWord('today is thrusday')).toEqual(3);
+        expect(countWord.strWord('Total words in a string')).toEqual(5);
+    });
+});
