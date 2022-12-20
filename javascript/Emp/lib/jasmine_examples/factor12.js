9577f0a0ff87a1ac0a2f98ee6f9828e5a17998f4
Monika Tilekar <monikatilekar80@gmail.com>
2022-12-20 20:47:35 +0530

    factor

---
+++ javascript/Emp/lib/jasmine_examples/factor12.js
@@ -0,0 +1,19 @@
+
+
+class Factors{
+
+    getFactors(number){
+        if(number<2){
+            return number+'';
+        }
+        let retVal = "1";
+
+        for(let divisor = 2; divisor <= number; divisor++) {
+            if(number%divisor == 0) {
+                retVal += "," + divisor ;
+            }    
+        }
+        return retVal;        
+    }
+}
+module.exports=Factors;
