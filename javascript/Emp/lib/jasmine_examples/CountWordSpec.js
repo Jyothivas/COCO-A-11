b72b6f0f5c5d8a11e7c2c4ca4d1bf99cf80fc129
Tilekar <118159006+Tilekar@users.noreply.github.com>
2022-12-20 21:21:35 +0530

    Update CountWordSpec.js

--- javascript/Emp/lib/jasmine_examples/CountWordSpec.js
+++ javascript/Emp/lib/jasmine_examples/CountWordSpec.js
@@ -6,6 +6,6 @@ describe('Count Word', function(){
         const countWord = new CountWord();
         expect(countWord.strWord('This project is done')).toEqual(4);
         expect(countWord.strWord('today is thrusday')).toEqual(3);
-        expect(countWord.strWord('Total words in a string')).toEqual(5);
+        expect(countWord.strWord('Total work complete')).toEqual(3);
     });
 });
