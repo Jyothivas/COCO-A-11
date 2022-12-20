93357601037596271d45a94fbf9a04cd0b73cb96
Monika Tilekar <monikatilekar80@gmail.com>
2022-12-20 20:38:39 +0530

    factorial

---
+++ javascript/Emp/lib/jasmine_examples/factorialspec.js
@@ -0,0 +1,14 @@
+const Factorial = require('../../lib/jasmine_examples/Factorial');
+
+describe('Factorial numbers',function(){
+    it('Addition of the factorial number',function(){
+        const factorial = new Factorial();
+        expect(factorial.sumOfFactorial(0)).toEqual('Enter greater than zero number');
+        expect(factorial.sumOfFactorial(1)).toEqual(1);
+        expect(factorial.sumOfFactorial(2)).toEqual(2);
+        expect(factorial.sumOfFactorial(3)).toEqual(6);
+        expect(factorial.sumOfFactorial(4)).toEqual(24);
+        expect(factorial.sumOfFactorial(5)).toEqual(120);
+        
+    });
+});
