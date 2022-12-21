0a66cf3c508f2a2c29e905a98b0ca253c7cfb9e4
Karan <karanmhetre9@gmail.com>
2022-12-21 11:06:29 +0530

    database connection

---
+++ html_css_db/server.js
@@ -0,0 +1,15 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const qbRoutes = require("./routes/qb");
+const cors = require("cors");
+
+
+const app = express();
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use(qbRoutes);
+
+app.listen(4000);
---
+++ html_css_db/utils/database.js
@@ -0,0 +1,60 @@
+const mysql = require("mysql2");
+
+const mysqlConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database:'employee_data'
+});
+
+mysqlConnection.connect((err) => {
+  if (!err) {
+    console.log("Connected");
+  } else {
+    console.log("Connection Failed");
+  }
+});
+
+module.exports = mysqlConnection;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const mysql = require('mysql2');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database:'employee_data'
+});
+
+connection.connect((error) => {
+  if(error){
+    console.log('Error connecting to the MySQL Database: '+error);
+    return;
+  }
+  console.log('Connection established sucessfully');
+});
+
+connection.query(`select * from employee`,(err,result,fields)=>{
+    if (err) {
+        console.log(err);
+    }
+    return console.log(result);
+})
+connection.end((error) => {
+}); */
