9ed2dbba5300b72ae0fad14a32e1cc825fcc9690
Karan <karanmhetre9@gmail.com>
2023-03-05 10:48:12 +0530

    first project changes in app components.

--- react-native/firstProject/App.tsx
+++ react-native/firstProject/App.tsx
@@ -1,29 +1,23 @@
-import { Image, ScrollView, Text, TextInput, View } from "react-native";
-import HelloWorldApp from "./HelloWorldApp";
-import TestRenderer from 'react-test-renderer';
+import { Image, Text, View } from "react-native";
 import GroceryShoppingList from "./components/GroceryShoppingList";
 
 function App() {
 
-
   return (
     <>
-      <ScrollView>
-        <View>
-          <Text>Some Text </Text>
-          <Text>Hello, World!</Text>
-          <Text>Some more text</Text>
-          <Image testID="cat-image" source={{ uri: 'https://reactnative.dev/docs/assets/p_cat2.png' }}
-            style={{ width: 200, height: 200 }}
-          />
-        </View>
-        <GroceryShoppingList />
-      </ScrollView>
+      <View>
+        <Text>Some Text </Text>
+        <Text>Hello, World!</Text>
+        <Text>Some more text</Text>
+        <Image
+          testID="cat-image"
+          source={{ uri: 'https://reactnative.dev/docs/assets/p_cat2.png' }}
+          style={{ width: 200, height: 200 }}
+        />
+      </View>
+      <GroceryShoppingList />
     </>
   );
 }
-
-// const testRenderer = TestRenderer.create(<App />);
-// const testInstance = testRenderer.root;
 
 export default App;
